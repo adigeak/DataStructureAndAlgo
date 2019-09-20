d22e2f2357d66a37f64532b05f3af4e1ebee82ac
Aditya Singh <31953737+adigeak@users.noreply.github.com>
2019-09-20 20:20:33 +0530

    Adding the comparison Between loop and function.

--- time.c
+++ time.c
@@ -11,9 +11,16 @@ int main()
 {
     // Calculate the time taken by fun()
     clock_t t;
+    //calling a function.
     t = clock();
     fun();
     t = clock() - t;
     double time_taken = ((double)t)/CLOCKS_PER_SEC;
     printf("function fun() took %f sec to execute \n", time_taken);
+    //Directly running the loop
+    t = clock();
+    for(int j = 0; i<=1000; j++);
+    t = clock() - t;
+    double time_taken = ((double)t)/CLOCKS_PER_SEC;
+    printf("loop took %f sec to execute \n", time_taken);
 }
